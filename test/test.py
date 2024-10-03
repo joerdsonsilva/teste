7f34eb85e632c4f237dc18aa726ec6af71c5f12e
joerdsonsilva <75455785+joerdsonsilva@users.noreply.github.com>
2024-10-03 20:29:00 -0300

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,18 +1,24 @@
+# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
+# SPDX-License-Identifier: Apache-2.0
+
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
-
+from cocotb.triggers import ClockCycles, RisingEdge, FallingEdge, Timer
+from cocotb.binary import BinaryValue
 
 @cocotb.test()
-async def test_modem(dut):
-    dut._log.info("start")
-    clock = Clock(dut.clk, 200, units="us")
+async def test_project(dut):
+    dut._log.info("Start")
+
+    # Set the clock period to 10 us (100 KHz)
+    clock = Clock(dut.clk, 200, units="ns")
     cocotb.start_soon(clock.start())
 
-    # reset
+  # reset
     dut._log.info("reset")
     dut.rst_n.value = 0
-    await Timer(200, units="us")
+    await ClockCycles(dut.clk, 500)
+    await Timer(200, units="ns")
 
     dut.rst_n.value = 1
     await ClockCycles(dut.clk, 1200)
